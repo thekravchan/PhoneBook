6c40570778ef9ac045f2c46fe0384d8975b3ebb3
thekravchan <thekravchan@gmail.com>
2022-11-27 05:31:25 +0700

    Mapping GET(records) request

--- src/main/java/com/bloodxtears/phonebook/controllers/PhoneBookController.java
+++ src/main/java/com/bloodxtears/phonebook/controllers/PhoneBookController.java
@@ -2,7 +2,11 @@ package com.bloodxtears.phonebook.controllers;
 
 import com.bloodxtears.phonebook.PhoneBookDao;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,5 +17,11 @@ public class PhoneBookController {
     @Autowired
     public PhoneBookController(PhoneBookDao phoneBook) {
         this.phoneBook = phoneBook;
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> getRecords(@RequestParam(defaultValue = "") String name,
+                                        @RequestParam(defaultValue = "") String phone) {
+        return ResponseEntity.ok(phoneBook.get(name,phone));
     }
 }
